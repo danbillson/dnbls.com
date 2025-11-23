95b4a700d007d0ccc1d50b97244aae255ce3a7b5
Dan Billson <dbillson@outlook.com>
2025-11-23 23:00:01 +0000

    feat: bricolage

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Work_Sans as FontSans } from "next/font/google";
+import { Bricolage_Grotesque as FontSans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: {
