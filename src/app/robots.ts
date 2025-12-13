78e958901e8a5dc6d0fc436ecfedcad1aba8e2e3
Dan Billson <dbillson@outlook.com>
2025-12-13 13:02:06 +0000

    meta

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: "https://dnbls.com/sitemap.xml",
+    host: "https://dnbls.com",
+  };
+}
