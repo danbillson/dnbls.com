3e9cac83c3143feb0083f53e715d9f2b2d374b29
Dan Billson <dbillson@outlook.com>
2026-07-25 09:24:25 +0100

    perf: remove priority on image

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -42,7 +42,6 @@ export default function Gallery() {
               height={image.height}
               className="rounded-lg"
               placeholder="blur"
-              priority
             />
           </motion.div>
         ))}
