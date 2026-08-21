ebeeab3cafda8de42365a2f14902b5c2581e24dd
Dan Billson <dbillson@outlook.com>
2026-08-21 07:07:50 +0100

    chore: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { fontMono, fontSans } from "@/lib/fonts";
 import "./globals.css";
@@ -52,6 +54,8 @@ export default function RootLayout({
             <Social />
           </MotionProvider>
         </main>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
